4415c9771397baa4853ee78d8dc62617ddd2f0bf
Dan <dan.pandolfo@gmail.com>
2021-10-01 11:12:04 -0400

    localstorage solution
    Added local storage, call back local storage and clear local storage functions and also created the list  to display them and a button to clear the localstorage

--- style/js/java.js
+++ style/js/java.js
@@ -1,3 +1,59 @@
+//Variable to hold all searched pokemon in an array
+var caughtPokemon=[];
+
+// displays the searched pokemon
+function createCaughtPokemon() {
+    var searchPokemon = document.querySelector("#search-pokemon").value;
+
+    caughtPokemon.push(searchPokemon);
+
+    var ulist = document.querySelector("#caught-ulist");
+    var savedPokemon = searchPokemon;
+    var li = document.createElement("caught-li");
+
+    // appends the searched pokemon to the list at the top of the page for Caught pokemon
+    li.className = 'list-group-item';
+    li.appendChild(document.createTextNode(savedPokemon));
+    ulist.appendChild(li);
+
+    //Locally stores the searched pokemon 
+    localStorage.setItem('caughtPokemon', JSON.stringify(caughtPokemon));
+}
+
+// return the pokemon from localstorage
+function returnLocalStorage() {
+    
+    var saved = JSON.parse(localStorage.getItem("caughtPokemon"));
+
+    if (!saved){
+        return false;
+    }
+
+    saved.push(caughtPokemon);
+    
+    for(i = 0; i < saved.length - 1; i++){
+        var pokemon = saved[i];
+        var ulist = document.querySelector("#caught-ulist");
+        var li = document.createElement("caught-li");
+
+        li.className = 'list-group-item';
+        li.appendChild(document.createTextNode(pokemon));
+        ulist.appendChild(li);
+        console.log(pokemon)
+        caughtPokemon.push(pokemon);
+    }
+}
+
+//when clicked clears localstorage
+function clearStorage(){
+    localStorage.clear();
+    caughtPokemon = [];
+    location.reload();    
+}
+
+
+
+//Function to call Api
 function getPokemon() {
     var searchPokemon = document.querySelector("#search-pokemon").value;
     var pokemonName = document.querySelector("#pokemon-name")
@@ -10,6 +66,7 @@ function getPokemon() {
         return false;
     }
 
+    // Fetch call
     fetch("https://pokeapi.co/api/v2/pokemon/" + searchPokemon.toLowerCase())
     .then(function(response){
         if (response.ok) {
@@ -22,6 +79,7 @@ function getPokemon() {
         }
     })
     .then(function(response){
+        //Attaching fetched image of pokemon to display on page
         pokemonName.innerHTML = searchPokemon;
         
         var imgEl = document.querySelector("#pokemon-image")
@@ -32,4 +90,10 @@ function getPokemon() {
 
         imgEl.appendChild(img);
     })
+    createCaughtPokemon()
+
+    document.querySelector("#search-pokemon").value = "";
 }
+
+//call returnLocalStorage function on load
+returnLocalStorage()
