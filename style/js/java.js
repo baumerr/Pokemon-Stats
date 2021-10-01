759b07b117b40da98b2c4be1cd61158259834720
Dan <dan.pandolfo@gmail.com>
2021-09-30 21:40:40 -0400

    made searchability with the api and search bar

--- style/js/java.js
+++ style/js/java.js
@@ -1,17 +1,29 @@
 function getPokemon() {
-    fetch("https://pokeapi.co/api/v2/pokemon/charmander")
+    var searchPokemon = document.querySelector("#search-pokemon").value;
+    var pokemonName = document.querySelector("#pokemon-name")
+    pokemonName.innerHTML = '';
+
+    console.log(searchPokemon)
+
+    if(!searchPokemon){
+        alert("You need to enter something")
+        return false;
+    }
+
+    fetch("https://pokeapi.co/api/v2/pokemon/" + searchPokemon.toLowerCase())
     .then(function(response){
-        if(response.ok) {
+        if (response.ok) {
             console.log(response)
             return response.json();
         }
         else {
-            alert('error: Not found');
-            return;
+            alert('Error: Not found');
+            return ;
         }
     })
     .then(function(response){
-        console.log(response)
+        pokemonName.innerHTML = searchPokemon;
+        
         var imgEl = document.querySelector("#pokemon-image")
 
         imgEl.innerHTML = '';
