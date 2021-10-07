ef080e363ac59ba4acbf1190d04f104eae683f40
Dan <dan.pandolfo@gmail.com>
2021-10-07 19:46:33 -0400

    added the modal to work for displaying searched pokemon

--- style/js/java.js
+++ style/js/java.js
@@ -62,7 +62,7 @@ function getPokemon(event) {
   var searchPokemon = document.querySelector("#search-pokemon").value;
   var pokemonName = document.querySelector("#pokemon-name");
   pokemonName.innerHTML = "";
-
+  modal.style.display = "block";
   console.log(searchPokemon);
 
   if (!searchPokemon) {
@@ -174,3 +174,25 @@ startSearch.addEventListener("submit", getPokemon);
 
 //call returnLocalStorage function on load
 returnLocalStorage();
+
+//---------------------------
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("user-submit");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
